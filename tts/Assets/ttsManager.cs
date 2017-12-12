e45dd3de260942df4eae939a485b23800ac9d9ae
Jingfei Lin <jade@Jingfeis-MBP.fios-router.home>
2017-12-11 23:46:43 -0500

    added tts

---
+++ tts/Assets/ttsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+using UnityEngine.UI;
+public class ttsManager : MonoBehaviour {
+
+	private string stringToEdit = "Type your wish here";
+	public InputField wishField;
+
+	// Use this for initialization
+	void Start () {
+		wishField.text = stringToEdit;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		wishField.text = stringToEdit;
+	}
+
+	public void Speak() {
+		EasyTTSUtil.SpeechAdd (stringToEdit);
+	}
+
+	public void RandomizeColor() {
+
+	}
+
+}
